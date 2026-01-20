f37f7cfa6a668a8e0fa7adadd32288548cbb884d
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2026-01-20 16:39:16 -0300

    chore: adjust root package.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="p-8 font-sans">
-      <h1 className="text-3xl font-bold">CRM</h1>
+      <h1 className="text-3xl text-red font-bold">CRM</h1>
       <p className="text-sm">Poppins deve estar aplicada aqui.</p>
     </div>
   )
